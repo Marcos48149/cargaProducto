ac5fd008724a9321bc4a74e168253a8f36c12295
marcos48149 <marcosgomez48149@gmail.com>
2026-06-16 11:10:04 -0300

    vuelvo a qwen/qwen2.5-vl-72b-instruct

--- vtex_loader.py
+++ vtex_loader.py
@@ -801,7 +801,7 @@ Devolvé SOLO UN JSON DE ESTA FORMA(EL JSON DE ACA ABAJO ES A MODO DE EJEMPLO PA
 }}"""
 
     response = client_oai.chat.completions.create(
-        model="google/gemini-2.0-flash-exp:free",
+        model="qwen/qwen2.5-vl-72b-instruct",
         messages=[
             {
                 "role": "user",
